4e2660b18737a8a36a3db36b13020cd2df718adf
Ekaterina Pogodina <pogodina@phystech.edu>
2019-08-29 19:35:50 +0300

    1

--- dcgan.py
+++ dcgan.py
@@ -17,8 +17,8 @@ class DCGAN(GAN):
         with tf.variable_scope("generator") as scope:
             dense1 = tf.layers.conv1d(z, filters=128, kernel_size=2, padding='same', activation='relu')
             dense2 = tf.layers.conv1d(dense1, filters=64, kernel_size=4, padding='same', activation='relu')
-            dense3 = tf.layers.conv1d(dense2, filters=32, kernel_size=4, padding='same', activation='relu')
-            dense3 = tf.reshape(dense3, shape=(self.batch_size, 32 * self.seq_length))
+            #dense3 = tf.layers.conv1d(dense2, filters=32, kernel_size=4, padding='same', activation='relu')
+            dense3 = tf.reshape(dense2, shape=(self.batch_size, 64 * self.seq_length))
             output = tf.layers.dense(dense3, units=self.seq_length)
 
         return tf.reshape(output, shape=(self.batch_size, self.seq_length, 1))
@@ -30,8 +30,8 @@ class DCGAN(GAN):
 
             dense1 = tf.layers.conv1d(x, filters=128, kernel_size=2, padding='same', activation='relu')
             dense2 = tf.layers.conv1d(dense1, filters=64, kernel_size=4, padding='same', activation='relu')
-            dense3 = tf.layers.conv1d(dense2, filters=32, kernel_size=4, padding='same', activation='relu')
-            dense3 = tf.reshape(dense3, shape=(self.batch_size, 32 * self.seq_length))
+            #dense3 = tf.layers.conv1d(dense2, filters=32, kernel_size=4, padding='same', activation='relu')
+            dense3 = tf.reshape(dense2, shape=(self.batch_size, 64 * self.seq_length))
             logits = tf.layers.dense(dense3, units=1)
 
         return logits
